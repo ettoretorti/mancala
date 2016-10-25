9290e52b52e6c17dc09c387f505d88dcfb09267a
Ettore Sergey Torti <ettoretorti@gmail.com>
2016-10-25 13:54:26 +0100

    Add tests for Board::makeMove()

--- test/board_tests.cpp
+++ test/board_tests.cpp
@@ -141,3 +141,140 @@ TEST(Board, ValidMoves) {
 	EXPECT_FALSE(movesCovered[0]); EXPECT_FALSE(movesCovered[2]);
 	EXPECT_FALSE(movesCovered[4]); EXPECT_FALSE(movesCovered[6]);
 }
+
+TEST(Board, SmallMove) {
+	Board b;
+	b.clear();
+
+	auto nHole = [&](size_t i) -> uint8_t& { return b.stonesInHole(NORTH, i); };
+	auto sHole = [&](size_t i) -> uint8_t& { return b.stonesInHole(SOUTH, i); };
+
+	sHole(6) = 2;
+	b.recalcMoves();
+
+	bool goAgain = b.makeMove(SOUTH, 6);
+
+	EXPECT_FALSE(goAgain);
+	EXPECT_EQ(1u, b.stonesInWell(SOUTH));
+	EXPECT_EQ(0u, b.stonesInWell(NORTH));
+	EXPECT_EQ(1u, nHole(0));
+
+	for(size_t i = 0; i < 7; i++) {
+		EXPECT_EQ(0u, sHole(i));
+	}
+
+	for(size_t i = 1; i < 7; i++) {
+		EXPECT_EQ(0u, nHole(i));
+	}
+
+	size_t nMoves;
+	const uint8_t* moves = b.validMoves(SOUTH, nMoves);
+
+	EXPECT_EQ(0u, nMoves);
+
+	moves = b.validMoves(NORTH, nMoves);
+	EXPECT_EQ(1u, nMoves);
+
+	EXPECT_EQ(0u, moves[0]);
+}
+
+TEST(Board, LastStoneInWell) {
+	Board b;
+	b.clear();
+
+	auto nHole = [&](size_t i) -> uint8_t& { return b.stonesInHole(NORTH, i); };
+	auto sHole = [&](size_t i) -> uint8_t& { return b.stonesInHole(SOUTH, i); };
+
+	nHole(0) = 7;
+	b.recalcMoves();
+
+	bool goAgain = b.makeMove(NORTH, 0);
+
+	EXPECT_TRUE(goAgain);
+	EXPECT_EQ(1u, b.stonesInWell(NORTH));
+	EXPECT_EQ(0u, b.stonesInWell(SOUTH));
+
+	EXPECT_EQ(0u, nHole(0));
+
+	for(size_t i = 0; i < 7; i++) {
+		EXPECT_EQ(0u, sHole(i));
+	}
+
+	for(size_t i = 1; i < 7; i++) {
+		EXPECT_EQ(1u, nHole(i));
+	}
+
+	size_t nMoves;
+	const uint8_t* moves = b.validMoves(SOUTH, nMoves);
+
+	EXPECT_EQ(0u, nMoves);
+
+	moves = b.validMoves(NORTH, nMoves);
+	EXPECT_EQ(6u, nMoves);
+
+	bool movesAvail[7] = { 0 };
+
+	for(size_t i = 0; i < nMoves; i++) {
+		movesAvail[moves[i]] = true;
+	}
+
+	EXPECT_FALSE(movesAvail[0]);
+	for(size_t i = 1; i < 7; i++) {
+		EXPECT_TRUE(movesAvail[i]);
+	}
+}
+
+TEST(Board, Capture) {
+	Board b;
+	b.clear();
+
+	auto nHole = [&](size_t i) -> uint8_t& { return b.stonesInHole(NORTH, i); };
+	auto sHole = [&](size_t i) -> uint8_t& { return b.stonesInHole(SOUTH, i); };
+
+	sHole(0) = 15;
+	nHole(6) = 69;
+	b.recalcMoves();
+
+	bool goAgain = b.makeMove(SOUTH, 0);
+
+	EXPECT_FALSE(goAgain);
+	EXPECT_EQ(72u, b.stonesInWell(SOUTH));
+	EXPECT_EQ(0u, b.stonesInWell(NORTH));
+
+	EXPECT_EQ(0u, sHole(0));
+	for(size_t i = 1; i < 7; i++) {
+		EXPECT_EQ(1u, sHole(i));
+	}
+
+	for(size_t i = 0; i < 6; i++) {
+		EXPECT_EQ(1u, nHole(i));
+	}
+	EXPECT_EQ(0u, nHole(6));
+
+	size_t nMoves;
+	const uint8_t* moves = b.validMoves(SOUTH, nMoves);
+	EXPECT_EQ(6u, nMoves);
+
+	bool movesAvail[7] = { 0 };
+	for(size_t i = 0; i < nMoves; i++) {
+		movesAvail[moves[i]] = true;
+	}
+
+	EXPECT_FALSE(movesAvail[0]);
+	for(size_t i = 1; i < 7; i++) {
+		EXPECT_TRUE(movesAvail[i]);
+	}
+
+	moves = b.validMoves(NORTH, nMoves);
+	EXPECT_EQ(6u, nMoves);
+
+	memset(movesAvail, 0, 7);
+	for(size_t i = 0; i < nMoves; i++) {
+		movesAvail[moves[i]] = true;
+	}
+
+	for(size_t i = 0; i < 6; i++) {
+		EXPECT_TRUE(movesAvail[i]);
+	}
+	EXPECT_FALSE(movesAvail[6]);
+}
