0240d4df058f29988d05a336e818c96a9406136d
Izz Abudaka <izz.abudaka@gmail.com>
2016-10-28 00:17:43 +0100

    fix minimax definition

--- src/mancala/MiniMaxAgent.hpp
+++ src/mancala/MiniMaxAgent.hpp
@@ -5,5 +5,6 @@
 class MiniMaxAgent : public Agent {
 public:
 	uint8_t makeMove(const Board& board, Side side, bool canSwitch);
-	uint8_t minimax(const Board& board, Side side, bool canSwitch);
+private:
+	uint8_t minimax(uint8_t depth, uint8_t curDepth, Side s, const Board& b, auto* moves);
 };
