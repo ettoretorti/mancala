ettoretorti/mancala

commit c31c9380be91cb69be29363db768d8c547a46621

Couldn't Compile (bug in OSX CLang). First Attempt at Minimax Bot

src/mancala/Board.cpp
@@ -46,6 +46,8 @@ void Board::reset() {
 	}
 	nScore_ = sScore_ = 0;
 	noNMoves_ = noSMoves_ = 7;
+	totalNStones_ = 49;
+	totalSStones_ = 49;
 }
 
 void Board::recalcMoves() {
@@ -66,6 +68,12 @@ uint8_t Board::stonesInHole(Side side, size_t holeNo) const {
 	assert(holeNo < 7);
 
 	return side == SOUTH ? sHoles_[holeNo] : nHoles_[holeNo];
+}
+
+uint8_t Board::totalStones(Side side) const {
+	assert(side == SOUTH || side == NORTH);
+	
+	return side == SOUTH ? totalNStones_ : totalSStones_;
 }
 
 uint8_t Board::stonesInWell(Side side) const {
@@ -107,6 +115,7 @@ void Board::removeMove(Side side, size_t holeNo) {
 	
 	uint8_t* arr    = side == SOUTH ? sMoves_ : nMoves_;
 	uint8_t& nMoves = side == SOUTH ? noSMoves_ : noNMoves_;
+	arr[holeNo] = arr[nMoves-1];
 
 	for(uint8_t i = 0; i < nMoves; i++) {
 		if(arr[i] == holeNo) {
@@ -122,6 +131,114 @@ void Board::placeAStone(Side curSide, uint8_t curHole){
 	if(stonesInHole(curSide, curHole) == 1) {
 		addMove(curSide, curHole);	
 	}
+}
+
+void Board::removeAStone(Side curSide, uint8_t curHole){
+	--stonesInHole(curSide, curHole);
+	if(stonesInHole(curSide, curHole) == 0) {
+		removeMove(curSide, curHole);	
+	}
+}
+
+bool Board::undoMove(Side side, size_t holeNo, uint8_t prevScore, uint8_t stones) {	
+	// ╔> 7 PLAYER HOLES ═> PLAYER WELL ═> 7 OPPONENT HOLES ╗
+	// ╚════════════════════════════════════════════════════╝
+
+	stonesInHole(side, holeNo) = stones;
+	if(stones < 15)
+		addMove(side, holeNo);
+
+	uint8_t curHole  = holeNo;
+	Side    curSide  = side;
+	bool goAgain = false;
+
+	if(stones > 15) {
+		uint8_t iterationsNum = stones % 15;
+		uint8_t perHole = stones / 15;
+
+		for(uint8_t i = 0; i < holeNo; i++) {
+			stonesInHole(curSide, i) -= perHole;
+			stonesInHole(opposite(curSide), i) -= perHole;
+			if(stonesInHole(curSide,i) == 0){
+				removeMove(curSide, i);
+			} 
+			if(stonesInHole(opposite(curSide),i) == 0){
+				removeMove(opposite(curSide), i);	
+			}
+		}
+		for(uint8_t i = holeNo+1; i < 7; i++) {
+			stonesInHole(curSide, i) -= perHole;
+			stonesInHole(opposite(curSide), i) -= perHole;
+			if(stonesInHole(curSide,i) == 0){
+				removeMove(curSide, i);
+			}
+			if(stonesInHole(opposite(curSide),i) == 0){
+				removeMove(opposite(curSide), i);	
+			}
+		}
+
+		stonesInWell(curSide) -= perHole;
+		uint8_t stonesLeft = iterationsNum;
+		// we cannot have empty capture as we already filled all holes
+		while(stonesLeft > 0){
+			if(curHole == 6){
+				if(curSide == side){
+					--stonesInWell(curSide);
+					stonesLeft--;
+
+					goAgain = stonesLeft == 0;
+				}
+
+				curHole = 0;
+				curSide = opposite(curSide);
+
+				if(stonesLeft > 0){
+					--stonesInHole(curSide, curHole);
+					stonesLeft--;
+				}
+			} else {
+				--stonesInHole(curSide, ++curHole);
+				stonesLeft--;
+			}
+		}
+	} else {
+		while(stones > 1){
+			if(curHole == 6){
+				if(curSide == side){
+					--stonesInWell(curSide);
+					stones--;
+				}
+
+				curHole = 0;
+				curSide = opposite(curSide);
+
+				if(stones > 0){
+					removeAStone(curSide, curHole);
+					stones--;
+				}
+			} else {
+				removeAStone(curSide, ++curHole);
+				stones--;
+			}
+		}
+
+		if(curHole < 6)
+			++curHole;
+		else{
+			curHole = 0;
+			curSide = opposite(curSide);
+		}
+
+		// Empty Hole Capture
+		if(stonesInHole(curSide, curSide) == 0){
+			stonesInHole(opposite(curSide), 6-curHole) += (stonesInWell(curSide) - prevScore);	
+			addMove(opposite(curSide), 6-curHole);
+			stonesInWell(curSide) -= (stonesInHole(opposite(curSide), 6-curHole) + 1);
+		} else{
+			removeAStone(curSide, curHole);
+		}
+	}
+	return goAgain;
 }
 
 bool Board::makeMove(Side side, size_t holeNo) {	

src/mancala/Board.hpp
@@ -17,6 +17,7 @@ public:
 	void reset();
 	void recalcMoves();
 
+	uint8_t totalStones(Side side) const;
 	uint8_t stonesInHole(Side side, size_t holeNo) const;
 	uint8_t stonesInWell(Side side) const;
 	
@@ -24,6 +25,7 @@ public:
 	uint8_t& stonesInWell(Side side);
 
 	bool makeMove(Side side, size_t holeNo);
+	bool undoMove(Side side, size_t holeNo, uint8_t prevScore, uint8_t stones);
 
 	const uint8_t* validMoves(Side side, size_t& nMoves) const;
 
@@ -40,8 +42,12 @@ private:
 	uint8_t sMoves_[7];
 	uint8_t noNMoves_;
 	uint8_t noSMoves_;
+	
+	uint8_t totalNStones_;
+	uint8_t totalSStones_;
 
 	void addMove(Side side, size_t holeNo);
 	void placeAStone(Side curSide, uint8_t curHole);
 	void removeMove(Side side, size_t holeNo);
+	void removeAStone(Side curSide, uint8_t curHole);
 };

src/mancala/MiniMaxAgent.cpp
@@ -1,0 +1,58 @@
+#include "MiniMaxAgent.hpp"
+
+uint8_t MiniMaxAgent::makeMove(const Board& b, Side s, bool canSwitch) {
+	size_t nMoves;
+	auto* moves = b.validMoves(s, nMoves);
+	if(nMoves == 0){
+		 if(b.totalStones() == 0)
+			return 1;
+		 else
+			return -1;
+	}
+	uint8_t depth = 5; // SOME DEPTH
+	return moves[minimax(depth, s, b, moves)];
+}
+
+uint8_t MiniMaxAgent::minimax(uint8_t depth, uint8_t curDepth, Side s, const Board& b, auto* moves){
+	// TODO: ACCOUNT FOR SWITCHES
+	Side& toMove = s;
+	uint8_t prevScore = b.stonesInWell(s);
+
+	if(s == SOUTH){
+		uint8_t maxScore = 0;
+		uint8_t bestMoveIndex = 0;
+		for(uint8_t i = 0; i < nMoves; i++) {
+			uint8_t stones = b.stonesInHole(s, moves[i]);
+			bool goAgain = b.makeMove(s, moves[i]);
+			if(!goAgain)
+				toMove = (Side)((int)s ^ 1);
+			uint8_t bestMove = minimax(depth, curDepth-1, toMove, b);
+			if(bestMove > maxScore){
+				maxScore = bestMove;
+				bestMoveIndex = i;
+			}
+			b.undoMove(s, moves[i], prevScore, stones);
+		}
+		if(depth == curDepth)
+			return bestMoveIndex;
+		return maxScore;
+	} else{
+		uint8_t minScore = 49; // or some max
+		uint8_t bestMoveIndex = 0;
+		for(uint8_t i = 0; i < nMoves; i++) {
+			uint8_t stones = b.stonesInHole(s, moves[i]);
+			bool goAgain = b.makeMove(s, moves[i]);
+			if(!goAgain)
+				toMove = (Side)((int)s ^ 1);
+			uint8_t bestMove = minimax(depth, curDepth-1, toMove, b);
+			if(bestMove < minScore){
+				minScore = bestMove;
+				bestMoveIndex = i;
+			}
+			b.undoMove(s, moves[i], prevScore, stones);
+		}
+		if(depth == curDepth)
+			return bestMoveIndex;
+		return minScore;
+	}
+}

src/mancala/MiniMaxAgent.hpp
@@ -1,0 +1,9 @@
+#pragma once
+
+#include "Agent.hpp"
+
+class MiniMaxAgent : public Agent {
+public:
+	uint8_t makeMove(const Board& board, Side side, bool canSwitch);
+	uint8_t minimax(const Board& board, Side side, bool canSwitch);
+};

commit 46ecb9c59982b6010e33281d2e07bc1207b15451

Fix bug introduced in c31c9380be9
Modifying Board::removeMove() caused a unit test to fail. This commit
reverts that change.

src/mancala/Board.cpp
@@ -115,7 +115,6 @@ void Board::removeMove(Side side, size_t holeNo) {
 	
 	uint8_t* arr    = side == SOUTH ? sMoves_ : nMoves_;
 	uint8_t& nMoves = side == SOUTH ? noSMoves_ : noNMoves_;
-	arr[holeNo] = arr[nMoves-1];
 
 	for(uint8_t i = 0; i < nMoves; i++) {
 		if(arr[i] == holeNo) {