4e77146fc541921523274a4547593b7f1d91d87f
Ettore Torti <ettore.torti@student.manchester.ac.uk>
2016-11-21 15:55:06 +0000

    Fix some bugs in MinimaxAgent

--- src/mancala/MiniMaxAgent.cpp
+++ src/mancala/MiniMaxAgent.cpp
@@ -1,19 +1,18 @@
 #include "MiniMaxAgent.hpp"
 
+#include <cassert>
+
+static uint8_t minimax(uint8_t depth, uint8_t curDepth, Side s, const Board& b, const uint8_t* moves, uint8_t nMoves);
 uint8_t MiniMaxAgent::makeMove(const Board& b, Side s, bool canSwitch) {
 	size_t nMoves;
 	auto* moves = b.validMoves(s, nMoves);
-	if(nMoves == 0){
-		 if(b.totalStones() == 0)
-			return 1;
-		 else
-			return -1;
-	}
+	assert(nMoves > 0);
+
 	uint8_t depth = 5; // SOME DEPTH
-	return moves[minimax(depth, depth, s, b, moves)];
+	return moves[minimax(depth, depth, s, b, moves, nMoves)];
 }
 
-uint8_t MiniMaxAgent::minimax(uint8_t depth, uint8_t curDepth, Side s, const Board& b, auto* moves){
+static uint8_t minimax(uint8_t depth, uint8_t curDepth, Side s, const Board& b, const uint8_t* moves, uint8_t nMoves){
 	// TODO: ACCOUNT FOR SWITCHES
 	Side& toMove = s;
 	uint8_t prevScore = b.stonesInWell(s);
@@ -26,7 +25,7 @@ uint8_t MiniMaxAgent::minimax(uint8_t depth, uint8_t curDepth, Side s, const Boa
 			bool goAgain = b.makeMove(s, moves[i]);
 			if(!goAgain)
 				toMove = (Side)((int)s ^ 1);
-			uint8_t bestMove = minimax(depth, curDepth-1, toMove, b);
+			uint8_t bestMove = minimax(depth, curDepth-1, toMove, b, nullptr, 0);
 			if(bestMove > maxScore){
 				maxScore = bestMove;
 				bestMoveIndex = i;
@@ -44,7 +43,7 @@ uint8_t MiniMaxAgent::minimax(uint8_t depth, uint8_t curDepth, Side s, const Boa
 			bool goAgain = b.makeMove(s, moves[i]);
 			if(!goAgain)
 				toMove = (Side)((int)s ^ 1);
-			uint8_t bestMove = minimax(depth, curDepth-1, toMove, b);
+			uint8_t bestMove = minimax(depth, curDepth-1, toMove, b, nullptr, 0);
 			if(bestMove < minScore){
 				minScore = bestMove;
 				bestMoveIndex = i;
--- src/mancala/MiniMaxAgent.hpp
+++ src/mancala/MiniMaxAgent.hpp
@@ -5,6 +5,4 @@
 class MiniMaxAgent : public Agent {
 public:
 	uint8_t makeMove(const Board& board, Side side, bool canSwitch);
-private:
-	uint8_t minimax(uint8_t depth, uint8_t curDepth, Side s, const Board& b, auto* moves);
 };
