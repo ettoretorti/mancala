836c5148e38e50b309a4f35a6a28984721dce29a
Izz Abudaka <izz.abudaka@gmail.com>
2016-11-26 16:15:14 +0000

    Alpha Beta - Minimax

--- src/mancala/MiniMaxAgent.cpp
+++ src/mancala/MiniMaxAgent.cpp
@@ -8,6 +8,7 @@
 *	index: The index of the best move. If the index == 7, then its a switch
 */
 static std::pair<uint8_t,double> minimax(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar);
+static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta);
 
 uint8_t MiniMaxAgent::makeMove(const Board& b, Side s, size_t movesSoFar, uint8_t lastMove) {
 	size_t nMoves;
@@ -16,7 +17,80 @@ uint8_t MiniMaxAgent::makeMove(const Board& b, Side s, size_t movesSoFar, uint8_
 
 	uint8_t depth = 7; // SOME DEPTH
 	Board bCopy = b;
-	return moves[minimax(depth, s, s, bCopy, movesSoFar).first];
+	return moves[minimax_alphabeta(depth, s, s, bCopy, movesSoFar, -1.0, 96.0).first];
+}
+
+static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta){
+	Side toMove = s;
+	size_t nMoves;
+	auto* moves = b.validMoves(toMove, nMoves);
+
+	if(depth == 0 || nMoves == 0)
+		return std::make_pair(0, b.totalStones(yourSide) - b.totalStones((Side)((int)yourSide ^ 1)));
+	// MAXIMIZE
+	if(toMove == yourSide){
+		std::pair<uint8_t, double> bestMove = std::make_pair(0, 0.0);
+		// switch, maximum
+		if(movesSoFar == 1){
+			Board bCopy = b;
+			yourSide = (Side)((int)yourSide ^ 1);
+			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta);
+			if(currentMove.second >= bestMove.second){
+				bestMove.first = 7;
+				bestMove.second = currentMove.second;
+				alpha = bestMove.second;
+			}
+			yourSide = (Side)((int)yourSide ^ 1);
+		}
+
+		for(uint8_t i = 0; i < nMoves; i++) {
+			Board bCopy = b;
+			bool goAgain = bCopy.makeMove(toMove, moves[i]);
+			if(!goAgain)
+				toMove = (Side)((int)toMove ^ 1);
+			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta);
+			if(currentMove.second >= bestMove.second){
+				bestMove.first = i;
+				bestMove.second = currentMove.second;
+				alpha = bestMove.second;
+				if(beta <= alpha){
+					break;
+				}
+			}
+		}
+		return bestMove;
+	// MINIMIZE
+	} else {
+		std::pair<uint8_t, double> bestMove = std::make_pair(0, 100.0); // SHOULD BE ADJUSTED TO MAX HEURISTIC
+		// switch, minimum
+		if(movesSoFar == 1){
+			Board bCopy = b;
+			yourSide = (Side)((int)yourSide ^ 1);
+			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta);
+			if(currentMove.second <= bestMove.second){
+				bestMove.first = 7;
+				bestMove.second = currentMove.second;
+				beta = bestMove.second;
+			}
+			yourSide = (Side)((int)yourSide ^ 1);
+		}
+		for(uint8_t i = 0; i < nMoves; i++) {
+			Board bCopy = b;
+			bool goAgain = bCopy.makeMove(toMove, moves[i]);
+			if(!goAgain)
+				toMove = (Side)((int)toMove ^ 1);
+			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta);
+			if(currentMove.second <= bestMove.second){
+				bestMove.first = i;
+				bestMove.second = currentMove.second;
+				beta = bestMove.second;
+				if(beta <= alpha){
+					break;
+				}
+			}
+		}
+		return bestMove;
+	}
 }
 
 static std::pair<uint8_t,double> minimax(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar){
