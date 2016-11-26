42e7d4edcf0fa49a0aa58ec0029400b21734bd7c
Izz Abudaka <izz.abudaka@gmail.com>
2016-11-26 15:46:41 +0000

    checking for terminating cases

--- src/mancala/MiniMaxAgent.cpp
+++ src/mancala/MiniMaxAgent.cpp
@@ -8,33 +8,26 @@
 *	index: The index of the best move. If the index == 7, then its a switch
 */
 static std::pair<uint8_t,double> minimax(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar);
-static double someHeuristic(Board& b, Side s);
 
 uint8_t MiniMaxAgent::makeMove(const Board& b, Side s, size_t movesSoFar, uint8_t lastMove) {
 	size_t nMoves;
 	auto* moves = b.validMoves(s, nMoves);
 	assert(nMoves > 0);
 
-	uint8_t depth = 5; // SOME DEPTH
+	uint8_t depth = 7; // SOME DEPTH
 	Board bCopy = b;
-
-	std::pair<uint8_t,double> result = minimax(depth, s, s, bCopy, movesSoFar);
-	return moves[result.first];
-}
-
-static double someHeuristic(Board& b, Side s){
-	return b.totalStones(s) - b.totalStones((Side)((int)s ^ 1));
+	return moves[minimax(depth, s, s, bCopy, movesSoFar).first];
 }
 
 static std::pair<uint8_t,double> minimax(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar){
-	if(depth == 0)
-		return std::make_pair(0, someHeuristic(b, yourSide));
 	Side toMove = s;
 	size_t nMoves;
+	auto* moves = b.validMoves(toMove, nMoves);
 
+	if(depth == 0 || nMoves == 0)
+		return std::make_pair(0, b.totalStones(yourSide) - b.totalStones((Side)((int)yourSide ^ 1)));
 	// MAXIMIZE
 	if(toMove == yourSide){
-		auto* moves = b.validMoves(toMove, nMoves);
 		std::pair<uint8_t, double> bestMove = std::make_pair(0, 0.0);
 		// switch, maximum
 		if(movesSoFar == 1){
@@ -63,7 +56,6 @@ static std::pair<uint8_t,double> minimax(uint8_t depth, Side s, Side yourSide, B
 		return bestMove;
 	// MINIMIZE
 	} else {
-		auto* moves = b.validMoves(s, nMoves);
 		std::pair<uint8_t, double> bestMove = std::make_pair(0, 100.0); // SHOULD BE ADJUSTED TO MAX HEURISTIC
 		// switch, minimum
 		if(movesSoFar == 1){
