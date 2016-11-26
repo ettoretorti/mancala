4e8dab038ec3bd22a8212a069768d79083c4f6fb
Izz Abudaka <izz.abudaka@gmail.com>
2016-11-26 16:30:31 +0000

    Added all Hueristics

--- src/mancala/MiniMaxAgent.cpp
+++ src/mancala/MiniMaxAgent.cpp
@@ -8,7 +8,9 @@
 *	index: The index of the best move. If the index == 7, then its a switch
 */
 static std::pair<uint8_t,double> minimax(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar);
-static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta);
+static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta, uint8_t goAgainsNum);
+static double scoreDiffHeuristic(Side yourSide, Board& b);
+static double stonesInFrontEmptyHolesHeuristic(Side yourSide, Board& b);
 
 uint8_t MiniMaxAgent::makeMove(const Board& b, Side s, size_t movesSoFar, uint8_t lastMove) {
 	size_t nMoves;
@@ -17,16 +19,30 @@ uint8_t MiniMaxAgent::makeMove(const Board& b, Side s, size_t movesSoFar, uint8_
 
 	uint8_t depth = 7; // SOME DEPTH
 	Board bCopy = b;
-	return moves[minimax_alphabeta(depth, s, s, bCopy, movesSoFar, -1.0, 96.0).first];
+	return moves[minimax_alphabeta(depth, s, s, bCopy, movesSoFar, -1.0, 96.0, 0).first];
 }
 
-static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta){
+static double stonesInFrontEmptyHolesHeuristic(Side yourSide, Board& b){
+	double total = 0.0;
+	for(int i = 0; i < 7; i++){
+		if(b.stonesInHole(yourSide, i) == 0){
+			total += b.stonesInHole((Side)((int)yourSide ^ 1), i);
+		}
+	}
+	return total;
+}
+
+static double scoreDiffHeuristic(Side yourSide, Board& b) {
+	return b.totalStones(yourSide) - b.totalStones((Side)((int)yourSide ^ 1));
+}
+
+static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta, uint8_t goAgainsNum){
 	Side toMove = s;
 	size_t nMoves;
 	auto* moves = b.validMoves(toMove, nMoves);
 
 	if(depth == 0 || nMoves == 0)
-		return std::make_pair(0, b.totalStones(yourSide) - b.totalStones((Side)((int)yourSide ^ 1)));
+		return std::make_pair(0, scoreDiffHeuristic(yourSide, b) + stonesInFrontEmptyHolesHeuristic(yourSide, b) + goAgainsNum);
 	// MAXIMIZE
 	if(toMove == yourSide){
 		std::pair<uint8_t, double> bestMove = std::make_pair(0, 0.0);
@@ -34,7 +50,7 @@ static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side y
 		if(movesSoFar == 1){
 			Board bCopy = b;
 			yourSide = (Side)((int)yourSide ^ 1);
-			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta);
+			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta, goAgainsNum);
 			if(currentMove.second >= bestMove.second){
 				bestMove.first = 7;
 				bestMove.second = currentMove.second;
@@ -46,9 +62,12 @@ static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side y
 		for(uint8_t i = 0; i < nMoves; i++) {
 			Board bCopy = b;
 			bool goAgain = bCopy.makeMove(toMove, moves[i]);
-			if(!goAgain)
+			if(!goAgain){
 				toMove = (Side)((int)toMove ^ 1);
-			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta);
+			} else{
+				goAgainsNum++;
+			}
+			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta, goAgainsNum);
 			if(currentMove.second >= bestMove.second){
 				bestMove.first = i;
 				bestMove.second = currentMove.second;
@@ -66,7 +85,7 @@ static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side y
 		if(movesSoFar == 1){
 			Board bCopy = b;
 			yourSide = (Side)((int)yourSide ^ 1);
-			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta);
+			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta, goAgainsNum);
 			if(currentMove.second <= bestMove.second){
 				bestMove.first = 7;
 				bestMove.second = currentMove.second;
@@ -77,9 +96,12 @@ static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side y
 		for(uint8_t i = 0; i < nMoves; i++) {
 			Board bCopy = b;
 			bool goAgain = bCopy.makeMove(toMove, moves[i]);
-			if(!goAgain)
+			if(!goAgain){
 				toMove = (Side)((int)toMove ^ 1);
-			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta);
+			} else{
+				goAgainsNum--;
+			}
+			std::pair<uint8_t, double> currentMove = minimax_alphabeta(depth-1, toMove, yourSide, bCopy, movesSoFar+1, alpha, beta, goAgainsNum);
 			if(currentMove.second <= bestMove.second){
 				bestMove.first = i;
 				bestMove.second = currentMove.second;
