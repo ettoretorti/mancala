ettoretorti/mancala

commit a084141be3497ffb45c5bc19f27e1ee5ca3521e7

Move reordering through shallow search

src/mancala/MiniMaxAgent.cpp
@@ -2,14 +2,16 @@
 #include <utility>
 #include <iostream>
 #include <cassert>
-
+#include <vector>
 /**
 *
 *	index: The index of the best move. If the index == 7, then its a switch
 */
 static std::pair<uint8_t,double> minimax(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar);
 static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta, uint8_t goAgainsNum);
+static void shallowSearch(uint8_t depth, Side s, Side yourSide, Board& b, size_t startMove, std::vector<std::pair<uint8_t, double>> possibleMoves, size_t movesSoFar);
 
+static bool pairCompare(const std::pair<uint8_t, double>& firstElem, const std::pair<uint8_t, double>& secondElem);
 static double scoreDiffHeuristic(Side yourSide, Board& b);
 static double stonesInFrontEmptyHolesHeuristic(Side yourSide, Board& b);
 static int stoneNumDiff(Side yourSide, Board& b);
@@ -21,12 +23,55 @@ uint8_t MiniMaxAgent::makeMove(const Board& b, Side s, size_t movesSoFar, uint8_
 	size_t nMoves;
 	auto* moves = b.validMoves(s, nMoves);
 	assert(nMoves > 0);
-
-	uint8_t depth = 10; // SOME DEPTH
+	Side toMove = s;
+	Side yourSide = s;
+	uint8_t depth = 10;
+	uint8_t sDepth = 5;
 	Board bCopy = b;
-	std::pair<uint8_t,double> result = minimax_alphabeta(depth, s, s, bCopy, movesSoFar, -1.0, 96.0, 0);
-	//std::cout << (int) s << ": " << (int) result.first << std::endl;
+
+	std::vector<std::pair<uint8_t, double>> possibleMoves( 7, std::make_pair(0, -1.0/0.0));
+	for(uint8_t i = 0; i < 7; i++){
+		Board nCopy = b;
+		if(i == 7 && movesSoFar == 1){
+			yourSide = (Side)((int)yourSide ^ 1);
+			shallowSearch(sDepth, toMove, yourSide, nCopy, i, possibleMoves, movesSoFar+1);
+			yourSide = (Side)((int)yourSide ^ 1);
+		}
+		else{
+			bool goAgain = nCopy.makeMove(toMove, i);
+			if(!goAgain)
+				toMove = (Side)((int)toMove ^ 1);
+			shallowSearch(sDepth, toMove, yourSide, nCopy, i, possibleMoves, movesSoFar+1);
+		}
+	}
+	
+	std::sort(possibleMoves.begin(), possibleMoves.end(), pairCompare);
+
+	std::pair<uint8_t,double> result = std::make_pair(0, -1.0/0.0);
+	for(uint8_t i = 0; i < 7; i++){
+		Board nCopy = b;
+		uint8_t move = possibleMoves[i].first;
+		if(move == 7 && movesSoFar == 1){
+			yourSide = (Side)((int)yourSide ^ 1);
+			std::pair<uint8_t,double> nResult = minimax_alphabeta(depth, toMove, yourSide, nCopy, movesSoFar+1, -1.0/0.0, 1.0/0.0, 0);
+			if(nResult.second > result.second)
+				result = nResult;
+			yourSide = (Side)((int)yourSide ^ 1);
+		}
+		else{
+			bool goAgain = nCopy.makeMove(toMove, move);
+			if(!goAgain)
+				toMove = (Side)((int)toMove ^ 1);
+			std::pair<uint8_t,double> nResult = minimax_alphabeta(depth, toMove, yourSide, nCopy, movesSoFar+1, -1.0/0.0, 1.0/0.0, 0);
+			if(nResult.second > result.second)
+				result = nResult;
+		}
+	}
 	return moves[result.first];
+}
+
+static bool pairCompare(const std::pair<uint8_t, double>& firstElem, const std::pair<uint8_t, double>& secondElem) {
+  return firstElem.first > secondElem.first;
 }
 
 static uint8_t overflow(Side yourSide, Board& b){
@@ -70,51 +115,80 @@ static double scoreDiffHeuristic(Side yourSide, Board& b) {
 	return b.stonesInWell(yourSide) - b.stonesInWell((Side)((int)yourSide ^ 1));
 }
 
+static void shallowSearch(uint8_t depth, Side s, Side yourSide, Board& b, size_t startMove, std::vector<std::pair<uint8_t, double>> possibleMoves, size_t movesSoFar){
+	Side toMove = s;
+	size_t nMoves;
+	auto* moves = b.validMoves(toMove, nMoves);
+	if(nMoves == 0){
+		uint8_t totalYourSide = b.stonesInWell(yourSide) + b.totalStones(yourSide);
+		uint8_t totalOtherSide = b.stonesInWell((Side)((int)yourSide ^ 1)) + b.totalStones((Side)((int)yourSide ^ 1));
+		if(totalYourSide > totalOtherSide)
+			possibleMoves[startMove] = std::make_pair(startMove, 1.0/0.0);
+		else if(totalYourSide == totalOtherSide && possibleMoves[startMove].second < 0.0)
+			possibleMoves[startMove] = std::make_pair(startMove,0.0);
+		return;
+	}
+	if(depth == 0){
+		int stoneNumDiff = b.totalStones(yourSide) - b.totalStones((Side)((int)yourSide ^ 1));
+		int scoreDiff = b.stonesInWell(yourSide) - b.stonesInWell((Side)((int)yourSide ^ 1));
+		if(possibleMoves[startMove].second < stoneNumDiff + scoreDiff)
+			possibleMoves[startMove] = std::make_pair(startMove, stoneNumDiff + scoreDiff);
+		return;
+	}
+	// MAXIMIZE
+	if(toMove == yourSide){
+		// switch, maximum
+		if(movesSoFar == 1){
+			Board bCopy = b;
+			yourSide = (Side)((int)yourSide ^ 1);
+			shallowSearch(depth-1, toMove, yourSide, bCopy, startMove, possibleMoves, movesSoFar+1);
+			yourSide = (Side)((int)yourSide ^ 1);
+		}
+
+		for(uint8_t i = 0; i < nMoves; i++) {
+			Board bCopy = b;
+			bool goAgain = bCopy.makeMove(toMove, moves[i]);
+			if(!goAgain)
+				toMove = (Side)((int)toMove ^ 1);
+			shallowSearch(depth-1, toMove, yourSide, bCopy, startMove, possibleMoves, movesSoFar+1);
+		}
+	// MINIMIZE
+	} else {
+		// switch, minimum
+		if(movesSoFar == 1){
+			Board bCopy = b;
+			yourSide = (Side)((int)yourSide ^ 1);
+			shallowSearch(depth-1, toMove, yourSide, bCopy, startMove, possibleMoves, movesSoFar+1);
+			yourSide = (Side)((int)yourSide ^ 1);
+		}
+		for(uint8_t i = 0; i < nMoves; i++) {
+			Board bCopy = b;
+			bool goAgain = bCopy.makeMove(toMove, moves[i]);
+			if(!goAgain)
+				toMove = (Side)((int)toMove ^ 1);
+			shallowSearch(depth-1, toMove, yourSide, bCopy, startMove, possibleMoves, movesSoFar+1);
+		}
+	}
+}
+
 static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta, uint8_t goAgainsNum){
 	Side toMove = s;
 	size_t nMoves;
 	auto* moves = b.validMoves(toMove, nMoves);
 	if(nMoves == 0){
-		if(b.stonesInWell(yourSide) > b.stonesInWell((Side)((int)yourSide ^ 1)))
+		uint8_t totalYourSide = b.stonesInWell(yourSide) + b.totalStones(yourSide);
+		uint8_t totalOtherSide = b.stonesInWell((Side)((int)yourSide ^ 1)) + b.totalStones((Side)((int)yourSide ^ 1));
+		if(totalYourSide > totalOtherSide)
 			return std::make_pair(0, 1.0/0.0);
-		else if(b.stonesInWell(yourSide) < b.stonesInWell((Side)((int)yourSide ^ 1)))
+		else if(totalYourSide < totalOtherSide)
 			return std::make_pair(0, -1.0/0.0);
 		else
-			return std::make_pair(0, 0);
+			return std::make_pair(0, 0.0);
 	}
 	if(depth == 0){
-		/*
-		int stoneNumDiff = b.stonesInWell(yourSide) - b.stonesInWell((Side)((int)yourSide ^ 1));
-		int scoreDiff = 0;
-		int stonesInFrontEmptyHoles = 0;
-		uint8_t overflow = 0;
-
-		for(int i = 0; i < 7; i++){
-			stoneNumDiff -= b.stonesInHole((Side)((int)yourSide ^ 1), i);
-			stoneNumDiff += b.stonesInHole(yourSide, i);
-			if(b.stonesInHole(yourSide, i) == 0){
-				stonesInFrontEmptyHoles += b.stonesInHole((Side)((int)yourSide ^ 1), i);
-			}
-			if(b.stonesInHole((Side)((int)yourSide ^ 1), i) == 0){
-				stonesInFrontEmptyHoles -= b.stonesInHole(yourSide, i);
-			}
-
-			uint8_t stonesLeft = b.stonesInHole(yourSide, i);
-			uint8_t currentHole = i;
-			while(stonesLeft > 0){
-				stonesLeft -= (7-currentHole);
-				if(stonesLeft > 6){
-					overflow += 6;
-					stonesLeft -= 6;
-					currentHole = 0;
-				} else if(stonesLeft > 0){
-					overflow += stonesLeft;
-					stonesLeft = 0;
-				}
-			}
-		}
-		*/
-		return std::make_pair(0, parameters[0]*scoreDiffHeuristic(yourSide, b) + parameters[1]*stoneNumDiff(yourSide, b) + parameters[2]*stonesInFrontEmptyHolesHeuristic(yourSide, b) - parameters[3]*overflow(yourSide, b));
+		int stoneNumDiff = b.totalStones(yourSide) - b.totalStones((Side)((int)yourSide ^ 1));
+		int scoreDiff = b.stonesInWell(yourSide) - b.stonesInWell((Side)((int)yourSide ^ 1));
+		return std::make_pair(0, parameters[0]*scoreDiff + parameters[1]*stoneNumDiff);
 	}
 	// MAXIMIZE
 	if(toMove == yourSide){

src/arena/main.cpp
@@ -5,9 +5,9 @@
 #include <iostream>
 
 int main() {
-	Agent* p1 = new MiniMaxAgent();
+	//Agent* p1 = new MiniMaxAgent();
 	//Agent* p1 = new UserAgent();
-	//Agent* p2 = new RandomAgent();
+	Agent* p1 = new RandomAgent();
 	Agent* p2 = new MiniMaxAgent();
 
 	Game g(p1, p2);

commit 87e89a3b1fae6f29a6fced4ea092cc58fefc3860

Fix compile error introduced in a084141be

src/mancala/MiniMaxAgent.cpp
@@ -1,8 +1,11 @@
 #include "MiniMaxAgent.hpp"
+
 #include <utility>
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <algorithm>
+
 /**
 *
 *	index: The index of the best move. If the index == 7, then its a switch