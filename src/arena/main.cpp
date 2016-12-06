e773157a3cafebf198cf6e264af0eb187ec63d51
Ettore Torti <ettore.torti@student.manchester.ac.uk>
2016-12-06 17:09:56 +0000

    Update arena
    - Show every intermediate move
    - Show timing information for each player

--- src/arena/main.cpp
+++ src/arena/main.cpp
@@ -1,5 +1,6 @@
 #include <mancala/Game.hpp>
 #include <mancala/RandomAgent.hpp>
+#include <mancala/MCAgent.hpp>
 #include <mancala/UserAgent.hpp>
 #include <mancala/MiniMaxAgent.hpp>
 #include <iostream>
@@ -10,13 +11,35 @@ int main() {
 	Agent* p1 = new RandomAgent();
 	Agent* p2 = new MiniMaxAgent();
 
+	bool printIntermediate = true;
+
 	Game g(p1, p2);
 	g.reset();
 
-	g.playAll();
+	g.takeTimings() = true;
 
-	std::cout << "Game finished in " << g.movesPlayed() << " moves.\n";
+	if(!printIntermediate) {
+		g.playAll();
+	} else {
+		while(!g.isOver()) {
+			std::cout << g.movesPlayed() << " moves played\n";
+			std::cout << (g.toMove() == SOUTH ? "SOUTH" : "NORTH") << " to move\n";
+			std::cout << "board state is:\n";
+			std::cout << g.board().toString();
+			g.stepTurn();
+			std::cout << "made move " << (int)g.lastMove() << "\n" << std::endl;
+		}
+		std::cout << g.movesPlayed() << " moves played\n";
+		std::cout << "game over\n";
+		std::cout << "board state is:\n";
+		std::cout << g.board().toString();
+		std::cout << std::endl;
+	}
+
+	std::cout << "Game finished in " << g.movesPlayed() << " moves\n";
 	std::cout << "The score difference is " << g.scoreDifference() << std::endl;
+	std::cout << "Player 1 took " << g.p1Time() << " seconds\n";
+	std::cout << "Player 2 took " << g.p2Time() << " seconds\n";
 
 	return 0;
 }
