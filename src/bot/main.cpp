392cb8ace55a1b4abc3eb3a5d7e0db46ebb2b61f
Ettore Sergey Torti <ettoretorti@gmail.com>
2016-12-10 10:34:38 +0000

    Tenative work on the bot

--- src/bot/main.cpp
+++ src/bot/main.cpp
@@ -1,3 +1,43 @@
+#include <mancala/input.hpp>
+#include <mancala/output.hpp>
+#include <mancala/Board.hpp>
+#include <mancala/RandomAgent.hpp>
+
+#include <memory>
+#include <iostream>
+
 int main() {
-	return 0;
+	using namespace std;
+	using namespace input;
+
+	auto agent = std::unique_ptr<Agent>(new RandomAgent());
+
+	auto msg = parseNext(cin);
+
+	Start* startMsg = dynamic_cast<Start*>(msg.get());
+
+	if(!startMsg) {
+		cerr << "Did not get start message!" << endl;
+		return -1;
+	}
+
+	Side curSide = SOUTH;
+	Side ourSide = startMsg->side;
+	Side oppSide = (Side)((int)curSide^1);
+	size_t movesPlayed =0 ;
+	Board b;
+	b.reset();
+
+	while(true) {
+		if(curSide == ourSide) {
+			uint8_t move = agent->makeMove(b, ourSide, movesPlayed, 0);
+			cout << output::move(move);
+
+			if(move >= 7) std::swap(curSide, oppSide);
+		}
+
+		msg = parseNext(cin);
+
+
+	}
 }
