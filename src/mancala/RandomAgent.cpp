4387ce0691145518de63934bf60d3b36ffabaf51
Ettore Sergey Torti <ettoretorti@gmail.com>
2016-12-11 17:36:32 +0000

    Optimize random number generation

--- src/mancala/RandomAgent.cpp
+++ src/mancala/RandomAgent.cpp
@@ -1,21 +1,42 @@
 #include "RandomAgent.hpp"
 
+#include <cassert>
 #include <random>
 
-static thread_local std::default_random_engine gen(std::random_device{}());
+static thread_local std::independent_bits_engine<std::default_random_engine, 1, uint8_t> jej(std::random_device{}());
+static thread_local std::independent_bits_engine<std::default_random_engine, 2, uint8_t> kek(std::random_device{}());
+static thread_local std::independent_bits_engine<std::default_random_engine, 3, uint8_t> lel(std::random_device{}());
+
+static uint8_t rand3() {
+	uint8_t s = kek();
+	while(s == 3) s = kek();
+
+	return s;
+}
+
+static uint8_t randBig(uint8_t n) {
+	uint8_t s = lel();
+	while(s >= n) s = lel();
+
+	return s;
+}
 
 uint8_t RandomAgent::makeMove(const Board& b, Side s, size_t movesSoFar, uint8_t lastMove) {
 	// 1/8 chance of switching when possible, since there will always be 7 other moves
 	if(movesSoFar == 1) {
-		std::uniform_int_distribution<uint8_t> dist(0, 7);
-		if(dist(gen) == 0) {
+		if(lel() == 0) {
 			return 7;
 		}
 	}
 
 	size_t nMoves;
 	auto* moves = b.validMoves(s, nMoves);
-	std::uniform_int_distribution<uint8_t> dist(0, nMoves-1);
+	assert(nMoves > 0);
+
+	if(nMoves == 1) return moves[0];
+	if(nMoves == 2) return moves[jej()];
+	if(nMoves == 3) return moves[rand3()];
+	if(nMoves == 4) return moves[kek()];
 	
-	return moves[dist(gen)];
+	return moves[randBig(nMoves)];
 }
