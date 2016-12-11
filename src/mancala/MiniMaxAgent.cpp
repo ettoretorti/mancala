3749e7db4743cb154af8d5476d9aac5531dc8e05
Izz Abudaka <izz.abudaka@gmail.com>
2016-12-11 16:46:21 +0000

    Added 49 stone max limit and fixed TODOs

--- src/mancala/MiniMaxAgent.cpp
+++ src/mancala/MiniMaxAgent.cpp
@@ -6,9 +6,6 @@
 #include <vector>
 #include <algorithm>
 
-/**
-*	index: The index of the best move. If the index == 7, then its a switch
-*/
 static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta, uint8_t goAgainsNum);
 
 static bool pairCompare(const std::pair<uint8_t, double>& firstElem, const std::pair<uint8_t, double>& secondElem);
@@ -37,22 +34,34 @@ static bool pairCompare_minimize(const std::pair<uint8_t, double>& firstElem, co
 }
 
 static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side yourSide, Board& b, size_t movesSoFar, double alpha, double beta, uint8_t goAgainsNum){
+	Side opponentSide = (Side)((int)yourSide ^ 1);
 	Side toMove = s;
 	size_t nMoves;
 	auto* moves = b.validMoves(toMove, nMoves);
 
+	// The Game is Actually Over
 	if(nMoves == 0){
-		// TODO: add stones in board
-		double totalYourSide = b.stonesInWell(yourSide) - b.stonesInWell((Side)((int)yourSide ^ 1));
+		double stonesOnBoardMax = b.totalStones(yourSide);
+		double stonesOnBoardMin = b.totalStones(opponentSide);
+		double totalYourSide = (b.stonesInWell(yourSide) + stonesOnBoardMax) - (b.stonesInWell(opponentSide) + stonesOnBoardMin);
+
+		double val = 0.0;
 		if(totalYourSide > 0)
-			return std::make_pair(0, 1.0/0.0);
+			val = 1.0/0.0;
 		else if(totalYourSide < 0)
-			return std::make_pair(0, -1.0/0.0);
-		else
-			return std::make_pair(0, 0.0);
+			val = -1.0/0.0;
+		return std::make_pair(0, val);
 	}
+
+	// Someone Can Reach A Certain Win
+	if(b.stonesInWell(yourSide) > 49)
+		return std::make_pair(0, 1.0/0.0);
+	else if(b.stonesInWell(opponentSide) > 49)
+		return std::make_pair(0, -1.0/0.0);
+
+	// We Have Reached the Maximum Depth
 	if(depth == 0)
-		return std::make_pair(0, b.stonesInWell(yourSide) - b.stonesInWell((Side)((int)yourSide ^ 1)));
+		return std::make_pair(0, b.stonesInWell(yourSide) - b.stonesInWell(opponentSide));
 	
 	// MAXIMIZE
 	if(toMove == yourSide){
@@ -61,23 +70,20 @@ static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side y
 			possibleMoves[i] = std::make_pair(moves[i], -1.0/0.0);
 		possibleMoves[nMoves] = std::make_pair(7, -1.0/0.0);
 
+		// Apply Move Reordering
 		if(depth > 3){
-			// HARDCODE swap
-			if(movesSoFar == 1){
-				yourSide = (Side)((int)yourSide ^ 1);
-				possibleMoves[nMoves].second = b.stonesInWell(yourSide) - b.stonesInWell((Side)((int)yourSide ^ 1));
-				yourSide = (Side)((int)yourSide ^ 1);
-			}
-			// Check other moves
+			// Check Swap
+			if(movesSoFar == 1)
+				possibleMoves[nMoves].second = b.stonesInWell(opponentSide) - b.stonesInWell(yourSide);
+			// Check All Moves
 			for(uint8_t i = 0; i < nMoves; i++){
 				Board nCopy = b;
 				bool goAgain = nCopy.makeMove(toMove, possibleMoves[i].first);
-				possibleMoves[i].second = nCopy.stonesInWell(yourSide) - nCopy.stonesInWell((Side)((int)yourSide ^ 1));
+				possibleMoves[i].second = nCopy.stonesInWell(yourSide) - nCopy.stonesInWell(opponentSide);
 			}
 
 			// Sort based on best payoff
-			// TODO don't consider bad moves
-			std::sort(std::begin(possibleMoves), std::end(possibleMoves), pairCompare);
+			std::sort(std::begin(possibleMoves), std::begin(possibleMoves)+nMoves+1, pairCompare);
 		}
 
 		std::pair<uint8_t,double> result = std::make_pair(8, -1.0/0.0);
@@ -89,8 +95,7 @@ static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side y
 			* I would have 99 problems, but a switch ain't one
 			*/
 			if(move == 7 && movesSoFar == 1){
-				yourSide = (Side)((int)yourSide ^ 1);
-				std::pair<uint8_t,double> nResult = minimax_alphabeta(depth-1, toMove, yourSide, nCopy, movesSoFar+1, alpha, beta, 0);
+				std::pair<uint8_t,double> nResult = minimax_alphabeta(depth-1, toMove, opponentSide, nCopy, movesSoFar+1, alpha, beta, 0);
 				if(nResult.second >= result.second){
 					result = nResult;
 					result.first = possibleMoves[i].first;
@@ -99,11 +104,10 @@ static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side y
 						break;
 					}
 				}
-				yourSide = (Side)((int)yourSide ^ 1);
 			}
 			else if(move < 7){
 				bool goAgain = nCopy.makeMove(toMove, move);
-				if(!goAgain)
+				if(!goAgain) 
 					toMove = (Side)((int)toMove ^ 1);
 				std::pair<uint8_t,double> nResult = minimax_alphabeta(depth-1, toMove, yourSide, nCopy, movesSoFar+1, alpha, beta, 0);
 				if(nResult.second >= result.second){
@@ -119,36 +123,35 @@ static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side y
 				assert(false);
 		}
 		return result;
+	} 
 	// MINIMIZE
-	} else {
+	else {
 		std::pair<uint8_t, double> possibleMoves[8];
 		for(uint8_t i = 0; i < nMoves; i++)
 			possibleMoves[i] = std::make_pair(moves[i], 1.0/0.0);
 		possibleMoves[nMoves] = std::make_pair(7, 1.0/0.0);
 
+		// Apply Move Reordering
 		if(depth > 3){
-			if(movesSoFar == 1){
-				yourSide = (Side)((int)yourSide ^ 1);
-				possibleMoves[nMoves].second = b.stonesInWell(yourSide) - b.stonesInWell((Side)((int)yourSide ^ 1));
-				yourSide = (Side)((int)yourSide ^ 1);
-			}
-			// Check other moves
+			// Check Swap
+			if(movesSoFar == 1)
+				possibleMoves[nMoves].second = b.stonesInWell(opponentSide) - b.stonesInWell(yourSide);
+			// Check All Moves
 			for(uint8_t i = 0; i < nMoves; i++){
 				Board nCopy = b;
 				bool goAgain = nCopy.makeMove(toMove, possibleMoves[i].first);
-				possibleMoves[i].second = nCopy.stonesInWell(yourSide) - nCopy.stonesInWell((Side)((int)yourSide ^ 1));
+				possibleMoves[i].second = nCopy.stonesInWell(yourSide) - nCopy.stonesInWell(opponentSide);
 			}
-
 			// Sort based on best payoff
-			std::sort(std::begin(possibleMoves), std::end(possibleMoves), pairCompare);
+			std::sort(std::begin(possibleMoves), std::begin(possibleMoves)+nMoves+1, pairCompare_minimize);
 		}
+
 		std::pair<uint8_t,double> result = std::make_pair(0, 1.0/0.0);
 		for(uint8_t i = 0; i < nMoves+1; i++){
 			Board nCopy = b;
 			uint8_t move = possibleMoves[i].first;
 			if(move == 7 && movesSoFar == 1){
-				yourSide = (Side)((int)yourSide ^ 1);
-				std::pair<uint8_t,double> nResult = minimax_alphabeta(depth-1, toMove, yourSide, nCopy, movesSoFar+1, alpha, beta, 0);
+				std::pair<uint8_t,double> nResult = minimax_alphabeta(depth-1, toMove, opponentSide, nCopy, movesSoFar+1, alpha, beta, 0);
 				if(nResult.second <= result.second){
 					result = nResult;
 					result.first = possibleMoves[i].first;
@@ -157,7 +160,6 @@ static std::pair<uint8_t,double> minimax_alphabeta(uint8_t depth, Side s, Side y
 						break;
 					}
 				}
-				yourSide = (Side)((int)yourSide ^ 1);
 			}
 			else if(move < 7){
 				bool goAgain = nCopy.makeMove(toMove, move);
