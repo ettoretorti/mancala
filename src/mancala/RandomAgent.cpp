c3afd82a6d3e850ea393d84e527f153f158e8e5d
Ettore Sergey Torti <ettoretorti@gmail.com>
2016-12-14 05:07:36 +0000

    RandomAgent optimization

--- src/mancala/RandomAgent.cpp
+++ src/mancala/RandomAgent.cpp
@@ -3,18 +3,19 @@
 #include <cassert>
 #include <random>
 
-static thread_local std::independent_bits_engine<std::default_random_engine, 1, uint8_t> jej(std::random_device{}());
-static thread_local std::independent_bits_engine<std::default_random_engine, 2, uint8_t> kek(std::random_device{}());
-static thread_local std::independent_bits_engine<std::default_random_engine, 3, uint8_t> lel(std::random_device{}());
 
-static uint8_t rand3() {
+static inline uint8_t randMid(uint8_t n) {
+	static thread_local std::independent_bits_engine<std::default_random_engine, 2, uint8_t> kek(std::random_device{}());
+
 	uint8_t s = kek();
-	while(s == 3) s = kek();
+	while(s >= n) s = kek();
 
 	return s;
 }
 
-static uint8_t randBig(uint8_t n) {
+static inline uint8_t randBig(uint8_t n) {
+	static thread_local std::independent_bits_engine<std::default_random_engine, 3, uint8_t> lel(std::random_device{}());
+
 	uint8_t s = lel();
 	while(s >= n) s = lel();
 
@@ -22,9 +23,11 @@ static uint8_t randBig(uint8_t n) {
 }
 
 uint8_t __attribute__((hot)) RandomAgent::makeMove(const Board& b, Side s, size_t movesSoFar, uint8_t lastMove) {
+	static thread_local std::independent_bits_engine<std::default_random_engine, 1, uint8_t> jej(std::random_device{}());
+
 	// 1/8 chance of switching when possible, since there will always be 7 other moves
 	if(movesSoFar == 1) {
-		if(lel() == 0) {
+		if(randBig(8) == 0) {
 			return 7;
 		}
 	}
@@ -35,8 +38,8 @@ uint8_t __attribute__((hot)) RandomAgent::makeMove(const Board& b, Side s, size_
 
 	if(nMoves == 1) return moves[0];
 	if(nMoves == 2) return moves[jej()];
-	if(nMoves == 3) return moves[rand3()];
-	if(nMoves == 4) return moves[kek()];
+	if(nMoves == 3) return moves[randMid(3)];
+	if(nMoves == 4) return moves[randMid(4)];
 	
 	return moves[randBig(nMoves)];
 }
